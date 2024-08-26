aaa75b504e2e6d0451340e33603949c86dbfb5bc
lalitmangale <lalitmangale25@gmail.com>
2024-08-26 17:29:02 +0530

    1st Project

---
+++ index.js
@@ -0,0 +1,46 @@
+var tablinks =  document.getElementsByClassName('tab-links');
+var tabcontents =  document.getElementsByClassName('tab-contents');
+
+function openTab(tabname) {
+   for( tablink of tablinks){
+    tablink.classList.remove("active-link")
+   }
+
+   for( tabcontent of tabcontents){
+    tabcontent.classList.remove("active-tab")
+   }
+
+   event.currentTarget.classList.add("active-link");
+   document.getElementById(tabname).classList.add("active-tab");
+}
+
+var sidemenu = document.getElementById("sidemenu");
+
+function openmenu() {
+   sidemenu.style.right = "0" ;
+}
+
+function closemenu() {
+   sidemenu.style.right = "-250px" ;
+}
+
+
+const scriptURL = 'https://script.google.com/macros/s/AKfycbx0VQ-Q4x1G3KhXhiKDueNS5dMQj-g1z8nqs80rhMbWgYcFeyV08JvS46XPOts7HcrLqg/exec'
+  const form = document.forms['submit-to-google-sheet']
+  const msg = document.getElementById("msg")
+
+
+
+  form.addEventListener('submit', e => {
+    e.preventDefault()
+    fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+      .then(response => {
+         msg.innerHTML = "Message Sent successfully"
+         setTimeout(function() {
+            msg.innerHTML = ""
+         }, 4000)
+         form.reset()
+      })
+
+      .catch(error => console.error('Error!', error.message))
+  })
